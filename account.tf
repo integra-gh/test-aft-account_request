700853069a52a24cad0a7189b9010697b398d4cf
josukutty <josukutty@integratech.ae>
2023-03-04 21:14:25 +0400

    network account contents

---
+++ account.tf
@@ -0,0 +1,29 @@
+module "networking_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "aws-test-ct-networking"
+    AccountName  = "Networking"
+    ManagedOrganizationalUnit = "Network"
+    SSOUserEmail     = "aws-test-ct-mgmt@integratech.ae"
+    SSOUserFirstName = "aws-test-ct"
+    SSOUserLastName  = "mgmt"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "josukutty"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "josukutty"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  a
+ccount_customizations_name = "name"
+}
